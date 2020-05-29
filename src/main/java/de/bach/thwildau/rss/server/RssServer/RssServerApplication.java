46174fc7373ea18e853bb23bc0f44ad34fb0bae4
Christian Bach <christian.bach@th-wildau.de>
2020-05-29 10:21:01 +0200

    War can be deploy.

--- src/main/java/de/bach/thwildau/rss/server/RssServer/RssServerApplication.java
+++ src/main/java/de/bach/thwildau/rss/server/RssServer/RssServerApplication.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +21,13 @@ import de.bach.thwildau.rss.server.operations.TagesschauNews;
 @Controller
 @EnableAutoConfiguration
 @SpringBootConfiguration
-public class RssServerApplication {
+@SpringBootApplication
+public class RssServerApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RssServerApplication.class);
+    }
 
 	@RequestMapping("/")
 	@ResponseBody
